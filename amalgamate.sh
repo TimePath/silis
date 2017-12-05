1b461cf30a958a5a073359f24d5c63f9c278c022
TimePath <andrew.hardaker1995@gmail.com>
2017-12-05 21:29:27 +1100

    amalgamate: simplify with -P

--- amalgamate.sh
+++ amalgamate.sh
@@ -2,11 +2,8 @@
 find src -name '*.c' | while read line;
     do echo "#include \"$line\"";
 done > silis.inc
-$CC -x c -E silis.inc > silis.txt
+$CC -x c -E -P silis.inc > silis.c
 rm silis.inc
-
-grep -v '^#' silis.txt > silis.c
-rm silis.txt
 
 echo '#define _Bool bool' > silis.cpp
 echo '#define NULL 0' >> silis.cpp
