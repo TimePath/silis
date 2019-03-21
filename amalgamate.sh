cc6b4cbf836332c704f8017b63d806bee38014c5
TimePath <andrew.hardaker1995@gmail.com>
2019-03-21 18:31:24 +1100

    Fix amalgamate.sh

--- amalgamate.sh
+++ amalgamate.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-find src -name '*.c' | while read line;
-    do echo "#include \"$line\"";
+find src -name '*.c' | while read line; do
+    if [ $line = 'src/test.c' ]; then continue; fi
+    echo "#include \"$line\"";
 done > silis.inc
 {
     echo '#define NULL 0'
     echo '#ifdef __cplusplus'
     echo '#define _Bool bool'
     echo '#endif'
-    $CC -x c -E -P -I $PWD/src silis.inc | perl -pe 's(\Q((void *) 0))(NULL)'
+    $CC -x c -E -P -I $PWD/src silis.inc | perl -pe 's/\Q((void *) 0)/NULL/g'
 } > silis.c
 rm silis.inc
