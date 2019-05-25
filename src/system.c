c2aeefc6df73d203afaca42d65f74396d07f5958
TimePath <andrew.hardaker1995@gmail.com>
2019-05-25 21:03:10 +1000

    libsystem: don't typedef libsystem_FILE twice

--- src/system.c
+++ src/system.c
@@ -9,38 +9,36 @@ DIAG_PUSH
 DIAG_IGNORE("-Wmissing-prototypes")
 DIAG_IGNORE("-Wdisabled-macro-expansion") // emscripten stdout macro
 
-typedef struct libsystem_FILE libsystem_FILE;
-
-libsystem_FILE *libsystem_fopen(const char *pathname, const char *mode) {
-    return (libsystem_FILE *) fopen(pathname, mode);
+struct libsystem_FILE *libsystem_fopen(const char *pathname, const char *mode) {
+    return (struct libsystem_FILE *) fopen(pathname, mode);
 }
 
-int libsystem_fseek(libsystem_FILE *stream, long offset, int whence) {
+int libsystem_fseek(struct libsystem_FILE *stream, long offset, int whence) {
     return fseek((FILE *) stream, offset, whence);
 }
 
-long libsystem_ftell(libsystem_FILE *stream) {
+long libsystem_ftell(struct libsystem_FILE *stream) {
     return ftell((FILE *) stream);
 }
 
-size_t libsystem_fread(void *ptr, size_t size, size_t nmemb, libsystem_FILE *stream) {
+size_t libsystem_fread(void *ptr, size_t size, size_t nmemb, struct libsystem_FILE *stream) {
     return fread(ptr, size, nmemb, (FILE *) stream);
 }
 
-size_t libsystem_fwrite(const void *ptr, size_t size, size_t nmemb, libsystem_FILE *stream) {
+size_t libsystem_fwrite(const void *ptr, size_t size, size_t nmemb, struct libsystem_FILE *stream) {
     return fwrite(ptr, size, nmemb, (FILE *) stream);
 }
 
-int libsystem_fflush(libsystem_FILE *stream) {
+int libsystem_fflush(struct libsystem_FILE *stream) {
     return fflush((FILE *) stream);
 }
 
-int libsystem_fclose(libsystem_FILE *stream) {
+int libsystem_fclose(struct libsystem_FILE *stream) {
     return fclose((FILE *) stream);
 }
 
-libsystem_FILE *libsystem_stdout(void) {
-    return (libsystem_FILE *) stdout;
+struct libsystem_FILE *libsystem_stdout(void) {
+    return (struct libsystem_FILE *) stdout;
 }
 
 char *libsystem_getcwd(char *buf, size_t size) {
